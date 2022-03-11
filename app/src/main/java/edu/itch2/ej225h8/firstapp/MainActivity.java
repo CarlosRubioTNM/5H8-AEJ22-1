cc349cbfeae46dc49a4dbabc2f82e05ffab644ad
CarlosRubioTNM <carlos.rr@chihuahua2.tecnm.mx>
2022-03-11 11:48:37 -0700

    Añadir evento.

--- app/src/main/java/edu/itch2/ej225h8/firstapp/MainActivity.java
+++ app/src/main/java/edu/itch2/ej225h8/firstapp/MainActivity.java
@@ -3,13 +3,26 @@ package edu.itch2.ej225h8.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnShowToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShowToast = findViewById(R.id.btnShowToast);
+        btnShowToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast t = Toast.makeText(getApplicationContext(),"Botón presionado", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        });
     }
 
 }
